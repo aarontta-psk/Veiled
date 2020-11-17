369ba197b586d232e35ccb736c66b83d821be82b
edekadt <edekadt@ucm.es>
2020-11-17 12:45:40 +0100

    El evento base es una imagen inerte

--- assets/scripts/sampleEvent.js
+++ assets/scripts/sampleEvent.js
@@ -1,3 +1,17 @@
 class Event extends Phaser.Scene{
+
+    constructor(){
+        super({key:'Event'});
+    }
+
+    preload(){
+        this.load.image('background', 'assets/sprites/city.png');
+    }
     
+    create(){
+        let background = this.add.image(0, 0, 'background');
+        background.setOrigin(0, 0);
+    }
 }
+
+export default Event;
