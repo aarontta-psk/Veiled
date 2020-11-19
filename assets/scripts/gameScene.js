51112774c4e818402fc0dddfc1d759dc0fc922b6
Aaron N. Moreno Sosa <aarmor01@ucm.es>
2020-11-19 13:13:46 +0100

    Añadido esqueleto metodo changeFrame
    Creado el player en la escena GameScene, correciones en la clase Player

--- assets/scripts/gameScene.js
+++ assets/scripts/gameScene.js
@@ -3,5 +3,7 @@ import Player from './player.js';
 export default class GameScene extends Phaser.Scene {
     constructor(){ super( { key: 'gameScene'} ) };
 
-
+    create() {
+        this.player = new Player(this, 100, 100);
+    }
 }
--- assets/scripts/player.js
+++ assets/scripts/player.js
@@ -3,10 +3,9 @@ export default class Player extends Phaser.GameObjects.Sprite {
         super(scene, x, y, 'player'); //llama a la constructora de Sprite
         this.scene.add.existing(this); //lo añades en la escena
         this.scene.physics.add.existing(this);
-        this.body.setCollideWorldBounds();
-        this.speedX = 300;
-        this.speedY = 300;
-        this.cursorsPlayer = this.scene.input.keyboard.addKeys({
+        this.body.setCollideWorldBounds(); //creamos limites fisicos
+        this.speed = 50; //velocidad
+        this.cursorsPlayer = this.scene.input.keyboard.addKeys({ //teclas de direccion
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
             left: Phaser.Input.Keyboard.KeyCodes.D,
@@ -20,9 +19,22 @@ export default class Player extends Phaser.GameObjects.Sprite {
     }
 
     update() {
-        if(this.cursorsPlayer.up.isDown) this.body.setVelocityY(speed);
-        else if(this.cursorsPlayer.down.isDown) this.body.setVelocityY(-speed);
-        else if(this.cursorsPlayer.left.isDown) this.body.setVelocity(-speed);
-        else this.body.setVelocity(speed);
+        if (this.cursorsPlayer.up.isDown) this.body.setVelocityY(speed);
+        else if (this.cursorsPlayer.down.isDown) this.body.setVelocityY(-speed);
+        else if (this.cursorsPlayer.left.isDown) this.body.setVelocityX(-speed);
+        else this.body.setVelocityX(speed);
+    }
+
+    changeFrame(value){
+        switch (value){
+            case 0: 
+            break;
+            case 1: 
+            break;
+            case 2: 
+            break;
+            case 3: 
+            break;
+        }
     }
 }
