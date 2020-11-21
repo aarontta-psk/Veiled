7809088c6487d4806271a0ab6d888b57b6f14523
Aaron N. Moreno Sosa <aarmor01@ucm.es>
2020-11-21 14:14:16 +0100

    El personaje ya se mueve bien
    Falta que funcionen las animaciones que bien yupi

--- assets/scripts/player.js
+++ assets/scripts/player.js
@@ -4,8 +4,8 @@ export default class Player extends Phaser.GameObjects.Sprite {
         this.scene.add.existing(this); //lo añades en la escena
         this.scene.physics.add.existing(this);
         this.body.setCollideWorldBounds(); //creamos limites fisicos
-        this.body.gravity.y = 0; //quitamos gravedad
-        this.speed = 50; //velocidad
+        this.body.allowGravity = false; //quitamos gravedad
+        this.speed = 100; //velocidad
         this.cursorsPlayer = this.scene.input.keyboard.addKeys({ //teclas de direccion
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
@@ -17,15 +17,29 @@ export default class Player extends Phaser.GameObjects.Sprite {
     preUpdate(time, delta) {
         super.preUpdate(time, delta);
         if (this.cursorsPlayer.up.isDown) {
-            this.body.setVelocityY(this.speed);
-            this.scene.player.anims.play('up_move', true);
+            this.body.setVelocityY(-this.speed);
+            this.body.setVelocityX(0);
+           // this.scene.player.anims.play('up_move', true);
         }
-        else if (this.cursorsPlayer.down.isDown) this.body.setVelocityY(-speed);
-        else if (this.cursorsPlayer.left.isDown) this.body.setVelocityX(-speed);
-        else if (this.cursorsPlayer.right.isDown) this.body.setVelocityX(speed);
+        else if (this.cursorsPlayer.down.isDown) {
+            this.body.setVelocityY(this.speed);
+            this.body.setVelocityX(0);
+            //this.scene.player.anims.play('up_move', true);
+        }
+        else if (this.cursorsPlayer.left.isDown) {
+            this.body.setVelocityY(0);
+            this.body.setVelocityX(-this.speed);
+           // this.scene.player.anims.play('up_move', true);
+        }
+        else if (this.cursorsPlayer.right.isDown) {
+            this.body.setVelocityY(0);
+            this.body.setVelocityX(this.speed);
+            //this.scene.player.anims.play('up_move', true);
+        }
         else {
             this.body.setVelocityX(0);
             this.body.setVelocityY(0);
+            //this.anims.stop();
         }
     }
 }
