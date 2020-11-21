01729b228a9e3fc062823bf07a9b8b8c8f846cdc
edekadt <edekadt@ucm.es>
2020-11-21 19:36:40 +0100

    Cambio de escena mediante Start

--- assets/scripts/eventManager.js
+++ assets/scripts/eventManager.js
@@ -1,10 +1,10 @@
-import event from './gameEventModule.js';
-
+import gameEvent from './gameEventModule.js';
+import mainScene from './gameScene.js';
 export default class EventManager extends Phaser.Scene
 {
     //Esta es la escena que carga, presenta y oculta los eventos
     constructor(){
-        super({key:'EventManager'});
+        super({key:'eventManager'});
     }
 
     preload(){
@@ -15,11 +15,19 @@ export default class EventManager extends Phaser.Scene
     create(){
         let background = this.add.image(0, 0, 'background2');
         background.setOrigin(0, 0);
+        this.gameEvent = new gameEvent('This is an event', 2, this);
+    }
+/*
+    closeEvent()
+        {
+            this.scene.switch(this, mainScene);
+        }*/
 
-        let title_text = this.add.text(100, 100, 'Event text');
-
-        let events = [
-            event1 = new event("This is an event.", 2, /*results*/ 2)
-        ];
+    update(time, delta){
+        if (this.gameEvent.isInteracting())
+        {
+            //closeEvent();
+            this.scene.start('gameScene');
+        }
     }
 }
--- assets/scripts/gameEventModule.js
+++ assets/scripts/gameEventModule.js
@@ -2,13 +2,23 @@ class Event
 {
     //Este es el evento base con todo lo que heredan los eventos individuales
     
-    constructor(eventText, eventOptions, eventResults)
+    constructor(eventText, eventOptions, sceneRef)
     {
-        this.text = text;
+        this.scene = sceneRef;
+        this.scene.add.existing(this);
+        this.scene.physics.add.existing(this);
+        
+        this.text = eventText;
         this.numOptions = eventOptions;
-        this.results = eventResults;
+
+        this.inputKeys = this.scene.input.keyboard.addKeys({ 
+            interact: Phaser.Input.Keyboard.KeyCodes.E
+        });
     }
    
+    isInteracting(){
+        return this.inputKeys.interact.isDown;
+    }
 }
 
 export default Event;
