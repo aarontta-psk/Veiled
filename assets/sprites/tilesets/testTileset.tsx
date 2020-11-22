bde8d3ec018c05ef3f59c3bede6b8f618145a0f4
Abel Moro <abelmoro@ucm.es>
2020-11-22 14:02:25 +0100

    starting tileset and map projects

---
+++ assets/sprites/tilesets/testTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="testTileset" tilewidth="32" tileheight="32" tilecount="320" columns="16">
+ <image source="testTileset.png" width="512" height="640"/>
+</tileset>
