82fde6a85e32668b6922a5a9459ec370f32d8aa9
Abel Moro <abelmoro@ucm.es>
2020-11-28 13:41:35 +0100

    added matter physics (disabled) and turn debug on

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,11 +9,24 @@ new Phaser.Game({
         mode: Phaser.Scale.NONE,
         autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
     },
+    /*physics: {
+        default: 'matter',
+        matter: {
+            enableSleeping: true,
+            gravity: {
+                y: 0
+            },
+            debug: {
+                showBody: true,
+                showStaticBody: true
+            }
+        }
+    */
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 300 },
-            debug: false
+            debug: true
         }
     },
     scene: [GameScene, EventManager],
