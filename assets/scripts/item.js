bc031729264a57d3684b01eef5f99ab6a447fe29
Abel Moro <abelmoro@ucm.es>
2020-12-06 07:17:33 +0100

    increased collider zone of items

--- assets/scripts/item.js
+++ assets/scripts/item.js
@@ -1,6 +1,13 @@
 export default class Item extends Phaser.Physics.Matter.Sprite {
     constructor(world, x, y, frame) {
         super(world, x, y, 'items', frame)
+
+        this.setBody({
+            type: 'rectangle',
+            width: 45,
+            height:45         
+        });  
+
         this.scene.add.existing(this);
         this.scene.matter.add.sprite(this);
         this.setStatic(true);
