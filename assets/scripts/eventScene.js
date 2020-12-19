1f568b9b9bf0cfeb7bca53dfd13ef9872b371e1c
edekadt <edekadt@ucm.es>
2020-12-19 18:58:45 +0100

    Events now have an opening description

--- assets/scripts/eventScene.js
+++ assets/scripts/eventScene.js
@@ -5,26 +5,29 @@ class eventScene extends Phaser.Scene {
 
     layout(options, group) {
         //variable para separar las opciones en el eje y
-        let distancia = 200;
+        let distancia = 300;
         //antes de mostrar las opciones, borro las anteriores
         group.removeAll(true);
+
+        this.add.text(50, 50, options[0].text, {fontFamily: 'Neucha', align: 'right'}).setResolution(2).setScale(2).setAlign('left'); 
+
         //para cada opcion
-        for (const actOption of options) {
+        for (let i = 1; i < options.length; i++) {
             console.log(options);
             //añado un texto
-            const optionText = this.add.text(50, distancia, actOption.text,{fontFamily: 'Neucha'}).setInteractive().setResolution(2).setScale(2);
+            const optionText = this.add.text(50, distancia, options[i].text,{fontFamily: 'Neucha'}).setInteractive().setResolution(2).setScale(2);
             //lo añado al container para borrarlo mas adelante
             group.add(optionText);
-            distancia += 100;
+            distancia += 75;
             
                 
             //llamo a un callback en caso de que sea pulsado
             optionText.on('pointerdown', () => {
-                if (actOption.condition === undefined || (actOption.condition !== undefined && actOption.condition(this)))
+                if (options[i].condition === undefined || (options[i].condition !== undefined && options[i].condition(this)))
                 {   
-                    actOption.cb();
+                    options[i].cb();
                     //si el evento continua, se llama de nuevo a la funcion
-                    if (actOption.next !== undefined) this.layout(actOption.next, group);
+                    if (options[i].next !== undefined) this.layout(options[i].next, group);
                     else {
                         this.scene.stop();
                         this.scene.run(this.info.prevScene.scene.key);
@@ -32,10 +35,9 @@ class eventScene extends Phaser.Scene {
                 }
                 else
                 {
-                    optionText.setText(actOption.failedText);
+                    optionText.setText(options[i].failedText);
                 }
             });
-            
         }
     }
 
@@ -65,6 +67,9 @@ export class testEvent extends eventScene {
         this.backgroundImage = 'background';
         this.content = [
             {
+                text: 'El leñador está cansado. Lleva todo el día depurando código en VSCode y está un poco hasta los huevos en realidad. Cómo le gustaría poder pasarse unas vacaciones relajadas para descansar y volver fuerte en enero para los exámenes, pero tiene que hacer malabares con proyectos simultáneos de c++, js y Blender. Le molaría bastante si le restaurases sus ganas de vivir.'
+            },
+            {
                 condition: function(ref){
                     return (ref.info.player.inventory.contains('pocion'))
                 },
@@ -89,11 +94,11 @@ export class testEvent extends eventScene {
                 ]
             },
             {
-                text: 'hacerle algo al jugador',
+                text: 'Do a barrel roll',
                 cb: () => {
                     console.log('opcion 3 pulsada');
                     console.log("jugador modificable, se ha pasado a esta escena", this.info.player)
-                    this.info.player.speed = 1;
+                    this.info.player.setScale(0.8, 0.8);
                 }
             },
             {
