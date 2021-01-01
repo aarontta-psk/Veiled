92e40f408df8227bcdd8b70ba5f023705fc8b60a
AaronNauzetMorenoSosa <aarmor01@ucm.es>
2021-01-01 17:48:08 +0100

    Added 3rd level base design
    Asking for opinion rn pls help

--- assets/scripts/silhouette.js
+++ assets/scripts/silhouette.js
@@ -1,11 +1,10 @@
 import Npc from "./npc.js";
-import NPC from "./npc.js"
 
 export default class Silhouette extends Npc {
     constructor(key, world, x, y, npcEvents, path) {
         super(key, world, x, y, npcEvents, path);
         
-
+        
     }
 
     activateSilhouette(blind) {
