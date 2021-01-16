b76b838bd6e7a59a8484fe7f8d1b2761e30db437
AaronNauzetMorenoSosa <aarmor01@ucm.es>
2021-01-16 15:24:00 +0100

    Improved tooltip creation on the GUI

--- assets/scripts/gui.js
+++ assets/scripts/gui.js
@@ -7,24 +7,9 @@ export default class GUI extends Phaser.GameObjects.Container {
         this.depth = 10;
 
         //Keybinds
-        this.keybindQ = this.scene.add.image(773, 95, 'keybindQ').setScrollFactor(0).setScale(0.6);
-        this.keybindQText = this.scene.add.text(665, 85, 'INVENTORY', {
-            fontFamily: 'Neucha',
-            color: '#ffffff'
-        }).setResolution(2).setScale(1.3).setScrollFactor(0).setDepth(11);
-        this.add(this.keybindQ);
-        this.keybindE = this.scene.add.image(773, 135, 'keybindE').setScrollFactor(0).setScale(0.6);
-        this.keybindEText = this.scene.add.text(675, 125, 'INTERACT', {
-            fontFamily: 'Neucha',
-            color: '#ffffff'
-        }).setResolution(2).setScale(1.3).setScrollFactor(0).setDepth(11);
-        this.add(this.keybindE);
-        this.keybindSpace = this.scene.add.image(743, 30, 'keybindSpace').setScrollFactor(0).setScale(0.5);
-        this.keybindSpaceText = this.scene.add.text(699, 50, 'BLINDFOLD', {
-            fontFamily: 'Neucha',
-            color: '#ffffff'
-        }).setResolution(2).setScale(1.3).setScrollFactor(0).setDepth(11);
-        this.add(this.keybindSpace);
+        this.addTooltip('keybindQ', 'INVENTORY', 773, 95, 665, 85, 0.6);
+        this.addTooltip('keybindE', 'INTERACT', 773, 135, 675, 125, 0.6);
+        this.addTooltip('keybindSpace', 'BLINDFOLD', 743, 30, 699, 50, 0.5);
 
         //Inventario
         this.inventoryRef = player.inventory;
@@ -34,11 +19,10 @@ export default class GUI extends Phaser.GameObjects.Container {
         this.backgroundInventory.setVisible(this.isVisible);
 
         //Texto del Item seleccionado
-        this.text = this.scene.add.text(15, 505, '', {
+        this.itemText = this.scene.add.text(15, 505, '', {
             fontFamily: 'Neucha',
             color: '#ffffff',
-        }).setResolution(2).setScale(1.3).setScrollFactor(0);
-        this.text.depth = 11;
+        }).setResolution(2).setScale(1.3).setScrollFactor(0).setDepth(11);
 
         //Barra cordura
         this.sanityBack = this.scene.add.image(150, 30, 'sanityBarBack').setScrollFactor(0);
@@ -59,7 +43,7 @@ export default class GUI extends Phaser.GameObjects.Container {
 
     //modificacion del texto del item seleccionado en GUI
     setInfoText(text) {
-        this.text.setText(text);
+        this.itemText.setText(text);
     }    
 
     //activar/desactivar inventario
@@ -100,7 +84,7 @@ export default class GUI extends Phaser.GameObjects.Container {
             console.log(item);
             item.setVisible(this.isVisible);
         }
-        this.text.setText('');
+        this.itemText.setText('');
     }
 
     //recolocacion dinamica de items (en caso de que se elimine uno)
@@ -110,10 +94,22 @@ export default class GUI extends Phaser.GameObjects.Container {
             item.setPosition(35 + (i * 42), 560);
             i++;
         }
-        this.text.setText('');
+        this.itemText.setText('');
     }
 
+    //metodo para actualizar la barra de cordura
     updateSanityBar(sanity) {
         this.sanityBar.scaleX = sanity / this.sanityTop;
+    }
+
+    //metodo para crear tooltips en la intefaz
+    addTooltip(keybind, text, x, y, xText, yText, scale){
+        let key = this.scene.add.image(x, y, keybind).setScrollFactor(0).setScale(scale);
+        let keyText = this.scene.add.text(xText, yText, text, {
+            fontFamily: 'Neucha',
+            color: '#ffffff'
+        }).setResolution(2).setScale(1.3).setScrollFactor(0).setDepth(11);
+        this.add(key);
+        this.add(keyText);
     }
 }
