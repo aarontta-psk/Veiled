d84985322e12ab35aef2f02c9befd1e4f1a98f27
iquintasALT <iquintas@ucm.es>
2021-01-21 21:45:40 +0100

    moved silhouette events to events_level1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,10 +7,10 @@ import Level1 from "./level1.js";
 import Level2 from "./level2.js";
 import InfoLevel from "./infoLevel.js";
 import PauseScene from "./pause_scene.js";
-import {testSilueta_0, testSilueta_1, testSilueta_2, testSilueta_3, testSilueta_4, testSilueta_5, testSilueta_6, testSilueta_7, testSilueta_8, testSilueta_9,
-     testSilueta_10, testSilueta_11} from "./event_scene.js";
 import {dad_Event_0, dad_Event_1, dad_Event_2} from "./events_level0.js"
-import {glassesItem_Event_0, glasses_Event_0, glasses_Event_1, glasses_Event_2, foreigner_Event_0, foreigner_Event_1, grandmother_Event_0, seller_Event_0,
+import {testSilueta_0, testSilueta_1, testSilueta_2, testSilueta_3, testSilueta_4, testSilueta_5, testSilueta_6, testSilueta_7, testSilueta_8, testSilueta_9,
+    testSilueta_10, testSilueta_11,
+    glassesItem_Event_0, glasses_Event_0, glasses_Event_1, glasses_Event_2, foreigner_Event_0, foreigner_Event_1, grandmother_Event_0, seller_Event_0,
 seller_Event_1, hungryKid_Event_0, elder_Event_0, elder_Event_1, tavern_Event_0, cane_Event_0, well_Event_0, coins_Event_0, grave_Event_0, brother_Event_Idle, brother_Event_0,
 doctor_Event_0, doctor_Event_1, doctor_Event_2, vagabond_Event_0, vagabond_Event_1, lumberjack_Event_0, lumberjack_Event_1,
 painterEvent_0, painterEvent_1, sickTree_Event_0, deathEvent_0, maxFaithEvent_0, doctor_Event_Idle, doctor_Event_Idle_1, doctor_Event_Idle_2, vagabond_Event_Idle, lumberjack_Event_Idle, sickTree_Event_Idle,
